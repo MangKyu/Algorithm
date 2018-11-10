040bde21b9965523036451196ad8feecac46b8bb
Mangkyu <whalsrb1226@naver.com>
2018-11-10 16:01:09 +0900

    11/10 Algorithm Complete

---
+++ src/Quiz10039.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Quiz10039 {
+    //https://www.acmicpc.net/problem/10039
+
+    public static void main(String args[]){
+        int arr[] = new int[5];
+        Scanner s = new Scanner(System.in);
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = s.nextInt();
+        }
+        int mean = getMeanScore(arr);
+        System.out.println(mean);
+    }
+
+    public static int getMeanScore(int arr[]){
+        int sum = 0;
+        for(int i = 0 ; i < arr.length; i++){
+            if(arr[i] < 40){
+                sum += 40;
+            }else {
+                sum += arr[i];
+            }
+        }
+        return sum/arr.length;
+    }
+
+
+
+}
