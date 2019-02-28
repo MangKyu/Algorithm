a73075d8b11c09d7deed383f16951bd88be1b4f0
MangKyu <enendekekd12>
2019-02-28 11:06:21 +0900

    Quiz 1107 Solved But should be Optimized

--- src/Quiz1107/Quiz1107.java
+++ src/Quiz1107/Quiz1107.java
@@ -3,220 +3,96 @@ package Quiz1107;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
-
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Quiz1107 {
 
-    //FIXME Quiz 1107 is not completed
     private static Set<Integer> errorSet;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int errorNum = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
         errorSet = new HashSet<>();
-        for (int i = 0; i < errorNum; i++) {
-            errorSet.add(Integer.parseInt(st.nextToken()));
-        }
 
-        Stack<Integer> numList = new Stack<>();
-
-        int tempN = N;
-        int startNum = 100;
-        int count = 0;
-
-        if (startNum != tempN) {
-            while (tempN != 0) {
-                int temp = tempN % 10;
-                tempN = tempN / 10;
-                numList.push(temp);
-            }
-
-            if (errorSet.size() != 10) {
-                count += numList.size();
-                int leftNum = 0;
-                int rightNum = 0;
-                boolean isEqual = true;
-
-                while (!numList.empty()) {
-                    int num = numList.pop();
-                    if (isEqual) {
-                        if (!errorSet.contains(num)) {
-                            leftNum = leftNum * 10 + num;
-                            rightNum = rightNum* 10 + num;
-                        } else {
-                            isEqual = false;
-
-                            int leftTemp = retrieveLeft(num);
-                            if (leftTemp == -1) {
-                                while(!isValid(++leftNum));
-                                leftTemp = retrieveLeft(9);
-                            }
-                            leftNum = leftNum * 10 + leftTemp;
-
-                            int rightTemp = retrieveRight(num);
-                            if(rightTemp == -1){
-
-                                while(!isValid(++rightNum));
-                                rightTemp = retrieveRight(0);
-                            }
-                            rightNum = rightNum * 10 + rightTemp;
-
-                        }
-                    }else{
-                        int leftTemp = retrieveLeft(9);
-                        leftNum = leftNum * 10 + leftTemp;
-
-                        int rightTemp = retrieveRight(0);
-                        rightNum = rightNum * 10 + rightTemp;
-
-                    }
-
-                    //System.out.println("Left: " + leftNum +  " Right: " + rightNum);
-                }
-
-                count += Math.min(N - leftNum , rightNum - N);
-                count = Math.min(Math.abs(N - 100), count);
-            }
-        }
-
-        System.out.println(count);
-
-    }
-
-    private static boolean isValid(int num){
-        Set<Integer> numSet = new HashSet<>();
-
-        while (num != 0) {
-            int temp = num % 10;
-            num = num / 10;
-            numSet.add(temp);
-        }
-        for (Integer integer : errorSet) {
-            if (numSet.contains(integer)) {
-                return false;
-            }
-        }
-        return true;
-
-    }
-    
-    private static int retrieveLeft ( int num){
-        for (int i = num; i >= 0; i--) {
-            if (!errorSet.contains(i)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    private static int retrieveRight ( int num){
-        for (int i = num; i < 10; i++) {
-            if (!errorSet.contains(i)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-}
-
-
-
-/*
-public class Quiz1107 {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int errorNum  = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        ArrayList<Integer> errorList = new ArrayList<>();
-        for(int i = 0 ; i < errorNum ; i++){
-            errorList.add(Integer.parseInt(st.nextToken()));
+        int errCnt = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        //에러가 0인 경우에는 입력을 받지 않음
+        if(errCnt!= 0){
+            st = new StringTokenizer(br.readLine());
+            // 에러 목록들을 초기화
+            for (int i = 0; i < errCnt; i++) {
+                errorSet.add(Integer.valueOf(st.nextToken()));
+            }
         }
 
-        Stack<Integer> numList = new Stack<>();
+        /* 주어진 수중에서 왼쪽과 가장 가까운 leftNum, 오른쪽과 가장 가까운 rightNum
+           그리고 +/-로만 이동하는 경우를 비교하는 알고리즘입니다. */
 
-
-        int tempN = N;
         int startNum = 100;
-        int count = 0;
+        int maxNum = 1000000;
+        int leftNum = -1;
+        int rightNum = maxNum;
 
-        if(startNum!=tempN){
-            while(tempN != 0){
-                int temp = tempN % 10;
-                tempN = tempN / 10;
-                numList.push(temp);
-            }
+        // +/- 버튼만으로 이동하는 경우로 cnt를 초기화해둡니다.
+        int cnt = Math.abs(N - startNum);
 
-            count += numList.size();
-            int leftNum = 0;
-            int rightNum = 0;
-
-
-            while(!numList.empty()){
-                int num = numList.pop();
-                boolean left = true;
-                boolean right = true;
-                for(int i = 0 ; i < 10 ; i++){
-
-                    if(!left && !right){
-                        System.out.println("Left: " + leftNum +  " Right: " + rightNum);
-                        break;
-                    }
-
-                    if(left){
-                        if(num - i >= 0 && !errorList.contains(num-i)){
-                            left = false;
-                            leftNum = leftNum * 10 + num - i;
-                            if(right && !(num + i <= 9 && !errorList.contains(num-i))){
-                                rightNum = rightNum * 10 + leftNum % 10 ;
-                                right = false;
-                            }
-                        }else if (num -i < 0){
-                            //left = false;
-                            if(!right){
-                                leftNum = leftNum * 10 + rightNum % 10 ;
-                            }
-                        }
-                    }
-
-                    if(right){
-                        if(num + i <= 9 && !errorList.contains(num-i)){
-                            right = false;
-                            rightNum = rightNum * 10 + num + i;
-                            if(left){
-                                leftNum = leftNum * 10 + rightNum % 10 ;
-                                left = false;
-                            }
-                        }else if (num + i > 9){
-                            //right = false;
-                            if(!left){
-                                rightNum = rightNum * 10 + leftNum % 10 ;
-                            }
-                        }
-                    }
+        if (N != startNum) {
+            for (int i = 0; i < maxNum; i++) {
+                if (i <= N && !hasError(i)) {
+                    leftNum = i;
+                } else if (i > N && !hasError(i)) {
+                    //오른쪽을 조회하는 경우는 더 커지면 조회하는 의미가 없으므로 반복문 종료
+                    rightNum = i;
+                    break;
                 }
             }
 
-            if(N - leftNum <= 0 && rightNum - N >= 0){
-                count += rightNum - N;
-            }else if ( rightNum - N <= 0 && N-leftNum >= 0){
-                count += N - leftNum;
-            }else{
-                count += Math.min(N - leftNum, rightNum - N);
+            // leftNum==-1인경우 좌측 숫자는 고려 x, rightNum==maxNum인 경우 우측 숫자는 고려 x
+            if (leftNum == -1 && rightNum != maxNum) {
+                cnt = Math.min(cnt, getNumSize(rightNum) + rightNum - N);
+            } else if (leftNum != -1 && rightNum == maxNum) {
+                cnt = Math.min(cnt, getNumSize(leftNum) + N - leftNum);
+            } else if (leftNum != -1 && rightNum != maxNum) {
+                int tempCnt = Math.min(getNumSize(leftNum) + N - leftNum, getNumSize(rightNum) + rightNum - N);
+                cnt = Math.min(cnt, tempCnt);
             }
 
         }
-
-        System.out.println(count);
-
+        System.out.println(cnt);
 
     }
 
+    // 해당 크기의 숫자를 반환합니다. 0일 경우에는 숫자를 쪼개지 않으므로 1을 반환하게 예외처리합니다.
+    private static int getNumSize(int num) {
+        int cnt = 0;
+        if (num == 0) {
+            cnt = 1;
+        } else {
+            while (num != 0) {
+                num /= 10;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    // 해당 숫자가 에러가 있는지 판단합니다. 0일 경우에는 반복문을 돌지 않으므로, 따로 처리해줍니다.
+    private static boolean hasError(int num) {
+        if (num == 0) {
+            return errorSet.contains(num);
+        } else {
+            while (num != 0) {
+                int temp = num % 10;
+                num /= 10;
+                if (errorSet.contains(temp)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
 }
-*/
