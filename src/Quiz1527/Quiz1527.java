22e6958e284c77e57cc4f77095b5ed1bd9ba42c6
mangkyu <whalsrb1226@naver.com>
2019-03-02 15:48:52 +0900

    Quiz 1527 Solved

--- src/Quiz1527/Quiz1527.java
+++ src/Quiz1527/Quiz1527.java
@@ -13,16 +13,27 @@ public class Quiz1527 {
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
 
+        int cnt = 0;
+
+        // A부터 B까지 탐색한다.
         for(int i = A ; i <= B ; i++){
-
+            if(isKeumManNumber(i)){
+                cnt++;
+            }
         }
-
+        System.out.println(cnt);
     }
 
-    private static void isKeumNum(int num){
-        //Set<Integer> numSet = new HashSet<>();
-
-
+    // 4나 7을 포함하고 있으면 바로 false를 return시킨다.
+    private static boolean isKeumManNumber(int num){
+        while(num != 0 ){
+            int temp = num % 10;
+            if(temp != 4 && temp != 7){
+                return false;
+            }
+            num /= 10;
+        }
+        return true;
     }
 
 }
