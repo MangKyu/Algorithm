a6a303958886e686152cbc6d24a5d68232212469
mangkyu <whalsrb1226@naver.com>
2019-03-02 14:19:14 +0900

    Quiz 1527 Solved

---
+++ src/Quiz1527/Quiz1527.java
@@ -0,0 +1,28 @@
+package Quiz1527;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Quiz1527 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        for(int i = A ; i <= B ; i++){
+
+        }
+
+    }
+
+    private static void isKeumNum(int num){
+        //Set<Integer> numSet = new HashSet<>();
+
+
+    }
+
+}
