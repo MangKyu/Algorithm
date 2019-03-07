279e23b923479f3ec2a08bc9688e47ef73e0f4a7
MangKyu <enendekekd12>
2019-03-07 22:45:07 +0900

    Quiz 1929 Solved

---
+++ src/Quiz1929/Quiz1929.java
@@ -0,0 +1,46 @@
+package Quiz1929;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Quiz1929 {
+
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+
+        for(int i=M; i<=N; i++) {
+            int divide = 2;
+            double root = Math.sqrt(i);
+            boolean primeNumberFlag = true;
+
+            if(i == 1) {
+                primeNumberFlag = false;
+            }
+
+            while(divide <= root) {
+
+                if(i%divide == 0) {
+                    primeNumberFlag = false;
+                    break;
+                }
+                divide++;
+            }
+            if(primeNumberFlag == true) {
+                bw.write(String.valueOf(i));
+                cnt++;
+                bw.newLine();
+            }
+        }
+        bw.flush();
+        System.out.println(cnt);
+    }
+
+}
