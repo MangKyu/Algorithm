ec29ee3c32138cc3e9430b5c100e42bd188d2d1e
MangKyu <enendekekd12>
2019-03-07 12:10:51 +0900

    Refactor Quiz 15552

--- src/SimpleQuiz/Quiz15552.java
+++ src/SimpleQuiz/Quiz15552.java
@@ -1,26 +1,20 @@
 package SimpleQuiz;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Quiz15552 {
 
-    public static void main(String args[]) {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        try {
-            int caseNum = Integer.parseInt(bf.readLine());
-            for(int i = 0; i < caseNum; i++){
-                String [] str = bf.readLine().split(" ");
-                int a = Integer.parseInt(str[0]);
-                int b = Integer.parseInt(str[1]);
-                bw.write(a+b +"\n");
-            }
-
-            bw.flush();
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int caseNum = Integer.parseInt(br.readLine());
+        for (int i = 0; i < caseNum; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
         }
+
+        System.out.println(sb.toString());
+
     }
 }
