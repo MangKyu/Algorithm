5d9b5459767c02d9a94714c4b0353689033961fd
MangKyu <whalsrb1226@naver.com>
2021-02-10 11:02:40 +0900

    Add PrintUtils

---
+++ src/Utils/PrintUtils.java
@@ -0,0 +1,27 @@
+package Utils;
+
+public class PrintUtils {
+
+    public static void print2ArrayFrom0(int[][] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void print2ArrayFrom1(int[][] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j < arr[0].length; j++) {
+                sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+}
